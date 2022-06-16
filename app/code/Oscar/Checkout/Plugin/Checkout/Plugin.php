7434d0f43817d9b457deb10275d454eb9d220657
Oscar <oscar_96vasa@hotmail.com>
2022-06-16 18:51:20 +0000

    RFC

---
+++ app/code/Oscar/Checkout/Plugin/Checkout/Plugin.php
@@ -0,0 +1,18 @@
+<?php
+namespace Oscar\Checkout\Plugin\Checkout;
+
+class Plugin
+{
+    
+    public function afterProcess(
+        \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
+        array $jsLayout
+    )
+    {
+        //RFC
+        $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']
+        ['shippingAddress']['children']['shipping-address-fieldset']['children']['vat_id']['label'] = 'RFC (Solo si requiere factura)';
+
+        return $jsLayout;
+    }
+}
